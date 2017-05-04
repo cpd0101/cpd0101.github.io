3309b59dd06e05dd8d45643f5b1274c079e83380
shaohui.tsh <shaohui.tsh@alibaba-inc.com>
2017-05-04 19:35:25 +0800

    添加stringify函数

---
+++ stringify.js
@@ -0,0 +1,48 @@
+const isPlainObject = require('lodash/isPlainObject')
+const cloneDeepWith = require('lodash/cloneDeepWith')
+
+const SPACE = ' '
+
+function map(obj, fn) {
+  return Object.keys(obj).reduce((output, key, index) => output.concat(fn(obj[key], key, index)), [])
+}
+
+function prettyPrintObject(obj, level, recursiveHandler) {
+  return `{${map(obj, (value, keyName) => `
+${SPACE.repeat(level + 1)}${!(/^[a-z$_][\w$]*$/i).test(keyName) ? `"${keyName.replace(/"/g, '\\"')}"` : keyName}: ${recursiveHandler(value, keyName, level + 1)}`).join(',')}
+${SPACE.repeat(level)}}`
+}
+
+function prettyPrintArray(arr, level, recursiveHandler) {
+  return `[${arr.map((value, keyName) => `
+${SPACE.repeat(level + 1)}${recursiveHandler(value, keyName, level + 1)}`).join(',')}
+${SPACE.repeat(level)}]`
+}
+
+function stringify(config, name, level = 0) {
+  if (typeof config === 'string') return `"${config.replace(/"/g, '\\"')}"`
+  if (
+    typeof config === 'number' ||
+    typeof config === 'boolean' ||
+    typeof config === 'undefined' ||
+    config === null
+  ) return String(config)
+
+  if (Array.isArray(config)) {
+    return prettyPrintArray(config, level, stringify)
+  }
+
+  if (isPlainObject(config)) {
+    return prettyPrintObject(config, level, stringify)
+  }
+
+  if (typeof config === 'function') {
+    return config.toString()
+  }
+
+  throw new Error({ message: `未知类型值：${config}` })
+}
+
+window.stringify = stringify
+
+module.exports = stringify
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 // Learn more on how to config.
 // - https://github.com/ant-tool/atool-build#配置扩展
 
+const webpack = require('webpack')
+
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push('transform-runtime');
   webpackConfig.babel.plugins.push(['import', {
     libraryName: 'antd',
     style: 'css',
   }]);
+
+  webpackConfig.plugins = webpackConfig.plugins.filter(plugin => !(plugin instanceof webpack.optimize.CommonsChunkPlugin))
 
   return webpackConfig;
 };
