70af9fd85033d4dcece24acebe859b290f95780f
shaohui.tsh <shaohui.tsh@alibaba-inc.com>
2019-10-04 10:37:33 +0800

    feat: 域名更换

--- proxy.js
+++ proxy.js
@@ -96,7 +96,7 @@
         '<span class="ads-close" style="position:absolute;top:0;right:-32px;width:28px;height:14px;line-height:14px;text-align:left;cursor:pointer;">' +
           '<div style="width:14px;font-size:12px;color:#555;background:#eee;text-align:center;">X</div>' +
         '</span>' +
-        '<iframe id="ads-iframe" border="0" frameborder="0" scrolling="no" marginwidth="0" allowtransparency="true" marginheight="0" src="https://www.proxyit.top/ads.html" />' +
+        '<iframe id="ads-iframe" border="0" frameborder="0" scrolling="no" marginwidth="0" allowtransparency="true" marginheight="0" src="https://www.anyproxy.cn/ads.html" />' +
       '</div>');
       adsHtml.on('click', function (e) {
         adsHtml.remove();
